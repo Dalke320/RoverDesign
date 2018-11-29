5b54f4553a88d8f0b15b3c1bb55329f1cf3063b9
Dalke320 <jacob.dalke@siu.edu>
2018-11-29 03:57:42 +0000

    Automation update

--- Code/autogit.py
+++ Code/autogit.py
@@ -1,11 +1,13 @@
 import pexpect
-
+import time
 try:
 	while 1:
 
 		child = pexpect.spawn("git add Heart1results.txt Heart2results.txt gpsresults.txt")
 		child = pexpect.spawn("git commit -m 'Log file update'")
 		child = pexpect.spawn("git push https://Dalke320:Roverdesign320@github.com/Dalke320/RoverDesign.git master")
+		print("Something is happening!")
+		time.sleep(1)
 
-	except KeyboardInterrupt:
-		child = pexpect.spawn("exit")
+except KeyboardInterrupt:
+	child = pexpect.spawn("exit")
