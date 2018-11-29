6af3bcac69a604c3fe2ee6d4a6849062a5e64a4e
Dalke320 <jacob.dalke@siu.edu>
2018-11-29 03:44:59 +0000

    Automation code

---
+++ Code/autogit.py
@@ -0,0 +1,11 @@
+import pexpect
+
+try:
+	while 1:
+
+		child = pexpect.spawn("git add Heart1results.txt Heart2results.txt gpsresults.txt")
+		child = pexpect.spawn("git commit -m 'Log file update'")
+		child = pexpect.spawn("git push https://Dalke320:Roverdesign320@github.com/Dalke320/RoverDesign.git master")
+
+	except KeyboardInterrupt:
+		child = pexpect.spawn("exit")
